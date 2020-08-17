2988c64757769a6d4d819f7e77801d35eca7e7ba
buihang2501 <buihang2501@gmail.com>
2020-08-17 11:21:57 +0700

    sig up UI

---
+++ SignUpConstrainLayout/app/src/main/java/hangbt/hust/signupconstrainlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package hangbt.hust.signupconstrainlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
