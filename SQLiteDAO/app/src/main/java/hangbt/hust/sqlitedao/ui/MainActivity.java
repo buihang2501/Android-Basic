a913b86668212136e9e4d51eb625c6e809a06d0e
buihang2501 <buihang2501@gmail.com>
2020-08-27 21:03:57 +0700

    update

--- SQLiteDAO/app/src/main/java/hangbt/hust/sqlitedao/ui/MainActivity.java
+++ SQLiteDAO/app/src/main/java/hangbt/hust/sqlitedao/ui/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        recyclerStudent = findViewById(R.id.recyclerStudent);
         recyclerStudent.setAdapter(studentApdater);
     }
 
